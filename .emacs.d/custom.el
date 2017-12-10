f59b04700b39f1a34a9d597fd5e751b6ae97f772
Siaw Young <lausiawyoung@gmail.com>
2017-12-10 16:05:34 +0800

    Re-add emacs.d

---
+++ .emacs.d/custom.el
@@ -0,0 +1,18 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("5673c365c8679addfb44f3d91d6b880c3266766b605c99f2d9b00745202e75f6" "8d3c5e9ba9dcd05020ccebb3cc615e40e7623b267b69314bdb70fe473dd9c7a8" "c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223" default)))
+ '(package-selected-packages
+   (quote
+    (smartparens go-mode easy-kill smooth-scroll gruvbox-theme use-package smart-mode-line projectile magit diff-hl counsel company aggressive-indent)))
+ '(smooth-scroll-mode t))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
---
+++ .emacs.d/init.el
@@ -0,0 +1,209 @@
+;; whoami
+(setq user-full-name "Lau Siaw Young"
+      user-mail-address "lausiawyoung@gmail.com")
+;; end
+
+;; load customize config
+(setq custom-file "~/.emacs.d/custom.el")
+(load custom-file)
+;; end
+
+;; add package archives
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(add-to-list 'package-archives '("org" . "https://orgmode.org/elpa/") t)
+(package-initialize)
+;; end 
+
+;; use-package
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+;; run recentf every 5 minutes
+(require 'recentf)
+(run-at-time (* 5 60) nil
+             (lambda ()
+               (let ((inhibit-message t))
+                 (recentf-save-list))))
+;; end
+
+(eval-and-compile
+  (defvar use-package-verbose t)
+  (require 'cl)
+  (require 'use-package)
+  (require 'bind-key)
+  (require 'diminish)
+  (setq use-package-always-ensure t))
+;; end
+
+;; validate config
+;; check for emacs config errors
+(use-package validate
+  :demand t)
+;; end
+
+;; prefer y/n
+(defalias 'yes-or-no-p 'y-or-n-p)
+;; end
+
+;; load $PATH env variable into emacs
+(use-package exec-path-from-shell
+  :demand t
+  :init (exec-path-from-shell-initialize))
+;; end
+
+;; set universal font size to 17px
+(set-face-attribute 'default nil :height 170)
+;; end
+
+;; Move all backup files to the OS's temp directory
+(setq backup-directory-alist
+      `((".*" . ,temporary-file-directory)))
+(setq auto-save-file-name-transforms
+      `((".*" ,temporary-file-directory t)))
+;; end
+
+;; Remove useless toolbars and splash screens
+(tooltip-mode -1)
+(tool-bar-mode -1)
+(menu-bar-mode -1)
+(scroll-bar-mode -1)
+(setq inhibit-splash-screen t)
+(setq inhibit-startup-message t)
+;; end
+
+;; Use single spaces
+(setq sentence-end-double-space nil)
+;; end
+
+;; Use 2 spaces for tabs
+(setq-default tab-width 2)
+(setq-default js-indent-level 2)
+(setq-default indent-tabs-mode nil)
+;; end
+
+;; stop blinking cursor
+(blink-cursor-mode 0)
+;; end
+
+;;;
+;; Third party package config
+;;;
+
+;; load gruvbox
+(load-theme 'gruvbox t)
+;; end
+
+;; counsel config
+(use-package counsel
+  :diminish ivy-mode
+  :bind
+  (("C-c C-r" . ivy-resume)
+   ("M-x" . counsel-M-x)
+   ("C-s" . swiper)
+   ("C-c i" . counsel-imenu)
+   ("C-x C-f" . counsel-find-file)
+   ("C-x j" . counsel-dired-jump)
+   ("C-x l" . counsel-locate)
+   ("C-c j" . counsel-git)
+   ("C-c s" . counsel-projectile-rg)
+   ("C-c f" . counsel-recentf)
+   ("M-y" . counsel-yank-pop)
+   :map swiper-map
+   ("C-r" . ivy-previous-line)
+   :map help-map
+   ("f" . counsel-describe-function)
+   ("v" . counsel-describe-variable)
+   ("l" . counsel-info-lookup-symbol)
+   :map ivy-minibuffer-map
+   ("C-d" . ivy-dired)
+   ("C-o" . ivy-occur)
+   ("<return>" . ivy-alt-done)
+   ("M-<return>" . ivy-immediate-done)
+   :map read-expression-map
+   ("C-r" . counsel-expression-history))
+  :init
+  (add-hook 'after-init-hook 'ivy-mode)
+  :config
+  (defun ivy-dired ()
+    (interactive)
+    (if ivy--directory
+        (ivy-quit-and-run
+         (dired ivy--directory)
+         (when (re-search-forward
+                (regexp-quote
+                 (substring ivy--current 0 -1)) nil t)
+           (goto-char (match-beginning 0))))
+      (user-error
+       "Not completing files currently")))
+  (setq counsel-find-file-at-point t)
+  (setq ivy-use-virtual-buffers t)
+  (setq ivy-display-style 'fancy)
+  (setq ivy-initial-inputs-alist nil)
+  (setq ivy-re-builders-alist
+        '((ivy-switch-buffer . ivy--regex-plus)
+          (swiper . ivy--regex-plus)
+          (t . ivy--regex-fuzzy)))
+  (ivy-set-actions
+   t
+   '(("I" insert "insert"))))
+;; end
+
+;; magit config
+(use-package magit
+  :bind (("s-g" . magit-status)))
+;; end
+
+;; projectile config
+(use-package projectile
+  :init
+  (add-hook 'after-init-hook 'projectile-mode)
+  :config
+  (setq projectile-use-git-grep t)
+  (setq projectile-completion-system 'ivy))
+;; end
+
+;; diff-hl config
+(use-package diff-hl)
+;; end
+
+;; company config
+(use-package company
+  :diminish company-mode
+  :bind
+  (:map company-active-map
+        ("M-n" . nil)
+        ("M-p" . nil)
+        ("C-n" . company-select-next)
+        ("C-p" . company-select-previous))
+  :init
+  (add-hook 'after-init-hook 'global-company-mode))
+;; end
+
+;; aggressive-indent config
+(use-package aggressive-indent
+  :diminish aggressive-indent-mode
+  :init
+  (add-hook 'after-init-hook 'aggressive-indent-global-mode))
+;; end
+
+;; smart-mode-line config
+(use-package smart-mode-line
+  :init
+  (add-hook 'after-init-hook 'sml/setup)
+  :config
+  (setq sml/theme 'respectful)
+  (setq sml/name-width 44)
+  (setq sml/shorten-directory t)
+  (setq sml/shorten-modes nil)
+  (setq sml/mode-width 'full))
+;; end
+
+;; easy-kill config
+(global-set-key [remap kill-ring-save] 'easy-kill)
+;; end
+
+;; smartparens default config
+(require 'smartparens-config)
+;; end
