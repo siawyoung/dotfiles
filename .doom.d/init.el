f47f10483022463539b0a77990213e364286fcf6
Siaw Young <lausiawyoung@gmail.com>
2019-06-30 22:14:56 +0800

    Change doom to vterm

--- .doom.d/init.el
+++ .doom.d/init.el
@@ -64,8 +64,8 @@
 
        :term
        ;;eshell            ; a consistent, cross-platform shell (WIP)
-       term              ; terminals in Emacs
-       ;;vterm             ; another terminals in Emacs
+       ;;term              ; terminals in Emacs
+       vterm             ; another terminals in Emacs
 
        :tools
        ;;ansible
