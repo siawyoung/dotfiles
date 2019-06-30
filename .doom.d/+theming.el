e5fd9c7dce36aff7d25790756340b96b47240ce2
Siaw Young <lausiawyoung@gmail.com>
2019-06-30 23:03:42 +0800

    Add solaire

--- .doom.d/+theming.el
+++ .doom.d/+theming.el
@@ -2,7 +2,13 @@
 
 (setq doom-font (font-spec :family "SF Mono" :size 16))
 
-(when IS-LINUX
-  (font-put doom-font :weight 'semi-light))
 (when IS-MAC
   (setq ns-use-thin-smoothing t))
+
+(load-theme 'doom-vibrant t)
+
+;; highlight active buffer
+(solaire-global-mode +1)
+(add-hook 'after-revert-hook #'turn-on-solaire-mode)
+(add-hook 'minibuffer-setup-hook #'solaire-mode-in-minibuffer)
+(solaire-mode-swap-bg)
