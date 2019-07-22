5bc44e156001242e87556c35a11f5486b8cf3c42
Siaw Young <sy@siawyoung.com>
2019-07-22 14:22:57 +0800

    Remove snippet support

--- .doom.d/init.el
+++ .doom.d/init.el
@@ -52,7 +52,7 @@
        ;;objed             ; text object editing for the innocent
        ;;parinfer          ; turn lisp into python, sort of
        ;;rotate-text       ; cycle region at point between text candidates
-       snippets          ; my elves. They type so I don't have to
+       ;;snippets          ; my elves. They type so I don't have to
 
        :emacs
        (dired            ; making dired pretty [functional]
