039b788873da9df545096d1dc110acc583f6c98f
Siaw Young <sy@siawyoung.com>
2021-12-11 13:49:20 +0800

    Remove solaire, add fix for emacs 29

--- doom/.doom.d/+theming.el
+++ doom/.doom.d/+theming.el
@@ -7,8 +7,3 @@
 (when IS-LINUX
   (setq doom-font (font-spec :family "Ubuntu Mono" :size 22)))
 
-;; highlight active buffer
-(solaire-global-mode +1)
-(add-hook 'after-revert-hook #'turn-on-solaire-mode)
-(add-hook 'minibuffer-setup-hook #'solaire-mode-in-minibuffer)
-(solaire-mode-swap-bg)
