a39323460a0fc470d28795084d5ff927ae5d01b2
Siaw Young <sy@siawyoung.com>
2025-04-04 17:02:01 +0800

    Update hammerspoon

--- mac/init.lua
+++ mac/init.lua
@@ -15,6 +15,36 @@ end)
 hs.hotkey.bind({"alt"}, "w", function()
     hs.application.launchOrFocus("Cursor")
 end)
+hs.hotkey.bind({"alt"}, "e", function()
+    hs.application.launchOrFocus("Google Chrome")
+end)
 
 hs.spoons.use("Lunette", { hotkeys = "default" })
 
+-- Open Vivaldi, then switch to third tab
+hs.hotkey.bind({"alt", "shift"}, "c", function()
+    hs.application.launchOrFocus("Vivaldi")
+
+    -- Give Vivaldi a moment to come into focus
+    hs.timer.doAfter(0.5, function()
+        hs.eventtap.keyStroke({"cmd"}, "3", 0)
+    end)
+end)
+
+hs.hotkey.bind({"alt", "shift"}, "m", function()
+    hs.application.launchOrFocus("Vivaldi")
+
+    -- Give Vivaldi a moment to come into focus
+    hs.timer.doAfter(0.5, function()
+        hs.eventtap.keyStroke({"cmd"}, "4", 0)
+    end)
+end)
+
+hs.hotkey.bind({"alt", "shift"}, "l", function()
+    hs.application.launchOrFocus("Vivaldi")
+
+    -- Give Vivaldi a moment to come into focus
+    hs.timer.doAfter(0.5, function()
+        hs.eventtap.keyStroke({"cmd"}, "5", 0)
+    end)
+end)
